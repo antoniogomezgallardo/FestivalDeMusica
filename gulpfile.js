23a1d274c3a311428956d7c0f2701201285887cb
Antonio Gómez Gallardo <antoniogomezgallardo@gmail.com>
2024-09-13 18:44:52 +0200

    Added watch mode to gulp by adding a watch function called dev to the gulpfile and modifying the script to be able to call it

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import {src, dest} from 'gulp'
+import {src, dest, watch} from 'gulp'
 import gulpSass from 'gulp-sass'
 import * as dartSass from 'sass'
 
@@ -10,4 +10,8 @@ export function css(done){
     .pipe(dest('build/css'))
 
     done();
+}
+
+export function dev(){
+    watch('src/scss/app.scss', css)
 }
