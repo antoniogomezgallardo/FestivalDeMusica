d8d6c99da551af5e0a980db4cc7ab12eafd31856
Antonio Gómez Gallardo <antoniogomezgallardo@gmail.com>
2024-09-13 18:37:19 +0200

    Added gulp-sass devdependency + script + configuration to compile sass with gulp from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-export function holaGulp(done){
-    console.log("---Hola desde gulpfile.js---");
+import {src, dest} from 'gulp'
+import gulpSass from 'gulp-sass'
+import * as dartSass from 'sass'
+
+const sass = gulpSass(dartSass);
+
+export function css(done){
+    src('src/scss/app.scss')
+    .pipe(sass())
+    .pipe(dest('build/css'))
 
     done();
 }
