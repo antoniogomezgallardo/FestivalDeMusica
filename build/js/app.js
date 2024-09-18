2513928074797da2149ff4b9b81a4f25fda586a9
Antonio Gómez Gallardo <antoniogomezgallardo@gmail.com>
2024-09-19 00:12:25 +0200

    Created the gallery section and added the images

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,20 @@
-alert("Hola Festivaleros!!")
+document.addEventListener('DOMContentLoaded', function () {
+    addGaleria();
+})
+
+function addGaleria() {
+
+    const divGaleria = document.querySelector('.contenido-galeria')
+    const NUMERO_DE_FOTOS = 16;
+
+    for (let i = 1; i <= NUMERO_DE_FOTOS; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen de la galeria'
+
+        divGaleria.appendChild(imagen)
+    }
+
+
+
+}
